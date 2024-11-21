7274cef8ec3a4ae62cadf35b5525f69dd271ee2a
rakeshSahani <sahanirakesh877@gmail.com>
2024-11-21 19:37:18 +0545

    dom purify locatio

--- src/components/SafeHTML.jsx
+++ src/components/SafeHTML.jsx
@@ -9,19 +9,19 @@
 
 // export default SafeHTML;
 
+import React from "react";
+// import DOMPurify from "dompurify";
+import DOMPurify from "https://cdn.jsdelivr.net/npm/dompurify@3.0.3/dist/purify.es.min.js";
 
-import React from 'react'
-import DOMPurify from "dompurify";
 
 const SafeHTML = ({ htmlString }) => {
-   const cleanHtmlString = DOMPurify.sanitize(htmlString);
+  const cleanHtmlString = DOMPurify.sanitize(htmlString);
 
   return (
     <>
       <div dangerouslySetInnerHTML={{ __html: cleanHtmlString }} />
     </>
-  )
-}
+  );
+};
 
-export default SafeHTML
-
+export default SafeHTML;
